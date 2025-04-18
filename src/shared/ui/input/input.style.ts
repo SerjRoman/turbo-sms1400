0feafb8be5af8a101db1428fd00bdf03e1b9850d
arman455 <snositel99@gmail.com>
2025-04-19 00:14:45 +0300

    Add Input component with styling and error handling

---
+++ src/shared/ui/input/input.style.ts
@@ -0,0 +1,66 @@
+import { StyleSheet } from "react-native"
+import { COLORS } from '../colors'
+
+export const styles = StyleSheet.create({
+    inputContainer: {
+        padding: 16,
+        width: 400,
+        height:80,
+    },
+    label: {
+        fontSize: 16,
+        color: COLORS.black,
+        marginBottom: 8,
+        paddingLeft:5,
+        fontWeight: '500',
+    },
+    inputWrapper: {
+        position: 'relative',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    leftIcon: {
+        position: 'absolute',
+        left: 16,
+        width: 36,
+        height: 36,
+        zIndex: 2,
+    },
+    rightIcon: {
+        position: 'absolute',
+        right: 16,
+        width: 25,
+        height: 25,
+        zIndex: 2,
+    },
+    input: {
+        flex: 1,
+        width: 349,
+        height: 60,
+        borderWidth: 1,
+        borderColor: COLORS.bisquePrimary,
+        borderRadius: 16,
+        fontSize: 16,
+        backgroundColor: COLORS.bisquePrimary,
+    },
+    inputWithLeftIcon: {
+        paddingLeft: 60, 
+    },
+    inputWithRightIcon: {
+        paddingRight: 60, 
+    },
+    errorText: {
+        color: COLORS.red,
+        fontSize: 14,
+        flexShrink: 1,
+        paddingLeft: 24,
+        fontWeight: '500',
+    },
+    errorIcon: {
+        position: 'absolute',
+        left: 10,
+        width: 15,
+        height: 15,
+        zIndex: 2,
+    },
+});
